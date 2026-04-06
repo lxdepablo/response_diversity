668e8cb5b0b315e584f9206ee57f97c3f8472842
Luis de Pablo <lude8513@login-ci5.rc.colorado.edu>
2026-04-06 12:51:04 -0600

    misc changes from alpine directory

--- code/generate_data_sbatch.sh
+++ code/generate_data_sbatch.sh
@@ -6,7 +6,7 @@
 #SBATCH --nodes=1 # Only use a single node
 #SBATCH --ntasks=64 # Run on 64 CPUs
 #SBATCH --mem=64gb # Memory limit
-#SBATCH --time=2:00:00 # Time limit hrs:min:sec
+#SBATCH --time=1:00:00 # Time limit hrs:min:sec
 #SBATCH --output=log.out # Standard output and error log
 #SBATCH --error=log.err # %j inserts job number
 
