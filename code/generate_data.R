d8844510064d328edaedfa507ede42f5a554d320
lxdepablo <luisxdepablo@gmail.com>
2026-04-06 12:43:07 -0600

    add linear p_contribute cases

--- code/generate_data.R
+++ code/generate_data.R
@@ -1,6 +1,7 @@
 # set working directory
 setwd("/projects/lude8513/response_diversity")
-#setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+#setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) # use to run locally
 #setwd("../")
 
 # load libraries
@@ -24,7 +25,7 @@ sd_range <- c(0.1, 30)
 # environmental gradient vector
 environment_vals <- seq(0, 100, 1)
 # how many ecosystems to generate
-n_simulations <- 1000
+n_simulations <- 1000 # set to 1000 for full results
 
 
 # generate data
@@ -43,16 +44,17 @@ linear_rand_int_ranges <- list(function_intercept_range, function_slope_range, c
 gaussian_constant_ranges <- list(function_intercept_range, function_slope_range, c(30, 30), b_range, c(10, 10), sd_range)
 gaussian_varied_ranges <- list(function_intercept_range, function_slope_range, c(0, 50), b_range, c(0, 20), sd_range)
 
-p_contribute_ranges <- list(function_intercept_range, function_slope_range, c(0, 50), b_range, c(0, 20), sd_range)
+p_contribute_gaussian_ranges <- list(function_intercept_range, function_slope_range, c(0, 50), b_range, c(0, 20), sd_range)
+p_contribute_linear_ranges <- list(function_intercept_range, function_slope_range, c(-100, 100), abundance_slope_range, sd_range)
 
 crossing_large_ranges <- list(function_intercept_range, function_slope_range, c(0, 100), c(-1, -1), sd_range)
 crossing_small_ranges <- list(function_intercept_range, function_slope_range, c(0, 100), c(-0.1, -0.1), sd_range)
 crossing_rand_ranges <- list(function_intercept_range, function_slope_range, c(-100, 100), c(-1, 1), sd_range)
 
 # generate data for proportion contribute sensitivity analysis
-p_contribute_n10_results <- do.call(rbind, lapply(proportion_contribute, function(p){
+p_contribute_gaussian_n10_results <- do.call(rbind, lapply(proportion_contribute, function(p){
   # run simulations for current value of p
-  curr_p <- run_n_sims(n_simulations, n_species = 10, environment_vals, p_contribute_ranges, response_shape = 'gaussian', p_contribute = p)
+  curr_p <- run_n_sims(n_simulations, n_species = 10, environment_vals, p_contribute_gaussian_ranges, response_shape = 'gaussian', p_contribute = p)
 
   #print(paste0(typeof(curr_p), " curr type"))
   #print(head(curr_p))
@@ -83,9 +85,9 @@ p_contribute_n10_results <- do.call(rbind, lapply(proportion_contribute, functio
   curr_stats
 }))
 
-p_contribute_n50_results <- do.call(rbind, lapply(proportion_contribute, function(p){
+p_contribute_gaussian_n50_results <- do.call(rbind, lapply(proportion_contribute, function(p){
   # run simulations for current value of p
-  curr_p <- run_n_sims(n_simulations, n_species = 50, environment_vals, p_contribute_ranges, response_shape = 'gaussian', p_contribute = p)
+  curr_p <- run_n_sims(n_simulations, n_species = 50, environment_vals, p_contribute_gaussian_ranges, response_shape = 'gaussian', p_contribute = p)
 
   # calc resilience
   resilience <- curr_p %>%
@@ -109,6 +111,66 @@ p_contribute_n50_results <- do.call(rbind, lapply(proportion_contribute, functio
   curr_stats
 }))
 
+p_contribute_linear_n10_results <- do.call(rbind, lapply(proportion_contribute, function(p){
+  # run simulations for current value of p
+  curr_p <- run_n_sims(n_simulations, n_species = 10, environment_vals, p_contribute_linear_ranges, response_shape = 'linear', p_contribute = p)
+  
+  #print(paste0(typeof(curr_p), " curr type"))
+  #print(head(curr_p))
+  
+  print("reached resilience calc")
+  
+  # calc resilience
+  resilience <- curr_p %>%
+    group_by(sim_number, E) %>%
+    summarize(total_function = sum(funct)) %>%
+    group_by(sim_number) %>%
+    summarize(resilience = 1/var(total_function))
+  
+  print("post resilience calc")
+  print(colnames(curr_p))
+  
+  # calc weighted response diversity
+  mean_weighted_response_diversity <- curr_p %>%
+    group_by(sim_number, species_ID) %>%
+    summarize(mean_abundance = mean(abundance), abundance_slope = median(abundance_slope), abundance_intercept = median(abundance_intercept)) %>%
+    group_by(sim_number) %>%
+    summarize(mean_weighted_response_diversity = Hmisc::wtd.var(abundance_slope, mean_abundance))
+  
+  curr_stats <- resilience %>%
+    left_join(mean_weighted_response_diversity, by = "sim_number") %>%
+    mutate(p = p)
+  
+  print(paste0("finished sim: p = ", p))
+  curr_stats
+}))
+
+p_contribute_linear_n50_results <- do.call(rbind, lapply(proportion_contribute, function(p){
+  # run simulations for current value of p
+  curr_p <- run_n_sims(n_simulations, n_species = 50, environment_vals, p_contribute_linear_ranges, response_shape = 'linear', p_contribute = p)
+  
+  # calc resilience
+  resilience <- curr_p %>%
+    group_by(sim_number, E) %>%
+    summarize(total_function = sum(funct)) %>%
+    group_by(sim_number) %>%
+    summarize(resilience = 1/var(total_function))
+  
+  # calc weighted response diversity
+  mean_weighted_response_diversity <- curr_p %>%
+    group_by(sim_number, species_ID) %>%
+    summarize(mean_abundance = mean(abundance), abundance_slope = median(abundance_slope), abundance_intercept = median(abundance_intercept)) %>%
+    group_by(sim_number) %>%
+    summarize(mean_weighted_response_diversity = Hmisc::wtd.var(abundance_slope, mean_abundance))
+  
+  curr_stats <- resilience %>%
+    left_join(mean_weighted_response_diversity, by = "sim_number") %>%
+    mutate(p = p)
+  
+  print(paste0("finished sim: p = ", p))
+  curr_stats
+}))
+
 # run simulations for main analyses
 linear_small_int_results <- run_n_sims(n_simulations, n_species = 10, environment_vals, linear_small_int_ranges, 'linear', p_contribute = 1)
 linear_mid_int_results <- run_n_sims(n_simulations, n_species = 10, environment_vals, linear_mid_int_ranges, 'linear', p_contribute = 1)
@@ -125,8 +187,10 @@ crossing_small_slope <- run_n_sims(n_simulations, n_species = 10, environment_va
 crossing_rand_slope <- run_n_sims(n_simulations, n_species = 10, environment_vals, crossing_rand_ranges, 'linear', p_contribute = 1, perfectly_crossing = TRUE)
 
 # write data to CSV's
-write_csv(p_contribute_n10_results, "data/sim_data/p_contribute_n10.csv")
-write_csv(p_contribute_n50_results, "data/sim_data/p_contribute_n50.csv")
+write_csv(p_contribute_gaussian_n10_results, "data/sim_data/p_contribute_gaussian_n10.csv")
+write_csv(p_contribute_gaussian_n50_results, "data/sim_data/p_contribute_gaussian_n50.csv")
+write_csv(p_contribute_linear_n10_results, "data/sim_data/p_contribute_linear_n10.csv")
+write_csv(p_contribute_linear_n50_results, "data/sim_data/p_contribute_linear_n50.csv")
 
 write_csv(linear_small_int_results, "data/sim_data/linear_small_int_results.csv")
 write_csv(linear_mid_int_results, "data/sim_data/linear_mid_int_results.csv")
